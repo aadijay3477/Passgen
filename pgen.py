2a878de4354d5c9e8bcfe3ef60eb18799b40e723
aadijay3477 <aadijay3477@gmail.com>
2022-01-31 06:39:30 +0000

    random_genretaor

--- pgen.py
+++ pgen.py
@@ -1,4 +1,10 @@
 import random
 lower="abcdefghijklmnopqrstuvw"
-upper=upper(lower)
-print(upper)
+upper=lower.upper()
+number="0123456789"
+symbol="[](){\}|'@#!$%^&*-+=_"
+
+all=lower+upper+number+symbol
+length=random.randint(8, 25)
+password="".join(random.sample(all, length))
+print(password)
